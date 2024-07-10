831dc5e140ae9f2b1d5c2af7d81423ec09a793ad
kunal04sinha <kunal.sinha32@gmail.com>
2024-07-10 22:11:53 +0530

    Feat: Added routing

---
+++ src/modules/NOC/NOC.jsx
@@ -0,0 +1,9 @@
+const NOC = () => {
+  return (
+    <div>
+      <h1>NOC</h1>
+    </div>
+  );
+};
+
+export default NOC;
--- src/modules/Resources/Resources.jsx
+++ src/modules/Resources/Resources.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { ReactTyped } from "react-typed";
 import Banner from "../../components/Banner";
 
 const Resources = () => {
