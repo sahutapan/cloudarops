e11a14ee6305ee39b01fde956a4d130b9d8c3932
kunalsinha04 <kunal.sinha32@gmail.com>
2024-07-15 11:07:26 +0530

    Fianlly Homepage is done

--- src/Components/layout/MainLayout.tsx
+++ src/Components/layout/MainLayout.tsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Headers from "../Headers";
 import React from "react";
 import Foter from "../Foter";
+import Lettalk from "../Lettalk";
 
 const MainLayout = () => {
   return (
     <>
       <Headers />
       <Outlet />
+      <Lettalk />
       <Foter />
     </>
   );
