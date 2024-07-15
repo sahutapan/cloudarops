720f31abb5fb369f436b86435a2edc036d7805b3
kunal04sinha <kunal.sinha32@gmail.com>
2024-07-15 22:55:33 +0530

    fix: resolved build issue

--- src/modules/Home/Home.jsx
+++ src/modules/Home/Home.jsx
@@ -1,12 +1,7 @@
 import HeroSection from "./components/HeroSection";
 import PassionateDevops from "./components/PassionateDevops";
 import Services from "./components/Services";
-import TechCard from "./components/TechCard";
 
-import TechImg1 from "../../assets/images/technologies-1-1024x435.png";
-import TechImg2 from "../../assets/images/technologies-2-1024x435.png";
-import TechImg3 from "../../assets/images/technologies-3-1024x435.png";
-import TechImg4 from "../../assets/images/technologies-4-1024x435.png";
 import Technologies from "./components/Technologies";
 import Oursuccess from "./components/Oursuccess";
 import DeliverWithExpertise from "./components/DeliverWithExpertise";
