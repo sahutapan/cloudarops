48fcfb92882f6c4dcc8f3bf5c55860eb3dfd2bb0
kunalsinha04 <kunal.sinha32@gmail.com>
2024-07-18 23:39:31 +0530

    Home Finnally done with responsive

--- src/modules/Home/components/Oursuccess.jsx
+++ src/modules/Home/components/Oursuccess.jsx
@@ -1,21 +1,21 @@
 import React from "react";
+
 const Oursuccess = () => {
   return (
-    <div className=" container mx-auto py-12 px-4 md:py-28 sm:px-6 lg:px-8">
+    <div className="container mx-auto py-12 px-4 md:py-28 sm:px-6 lg:px-8">
       <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold text-center mb-4 text-black">
         Our <span className="text-blue-500">success</span> stories
       </h2>
       <p className="text-center mb-8 text-base sm:text-lg">
         Here’s how we have helped our clients to overcome the DevOps challenges
       </p>
-      <div className=" grid grid-cols-1 md:grid-cols-3 px-10  gap-10 md:px-40  justify-center pt-12 pb-12">
-        <div className=" bg-white rounded-xl shadow-2xl p-6 flex flex-col items-center text-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-4 md:px-6 lg:px-8">
+        <div className="bg-white rounded-xl shadow-2xl p-6 flex flex-col items-center text-center">
           <h3 className="text-lg sm:text-xl font-semibold mb-4">
             Complete infrastructure in{" "}
             <span className="font-bold text-blue-500">AWS</span>
           </h3>
-
-          <p className="text-gray-700 mb-6 text-sm sm:text-base md:pt-12">
+          <p className="text-gray-700 mb-6 text-sm sm:text-base">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum alias
             et harum neque nisi aliquam impedit deserunt? Est, nesciunt debitis
             mollitia similique dolorem nihil tenetur, error inventore porro
@@ -25,17 +25,12 @@ const Oursuccess = () => {
             Read More
           </a>
         </div>
-        <div className="bg-white rounded-lg shadow-2xl p-6 flex flex-col items-center text-center">
+        <div className="bg-white rounded-xl shadow-2xl p-6 flex flex-col items-center text-center">
           <h3 className="text-lg sm:text-xl font-semibold mb-4">
             Migration from{" "}
             <span className="font-bold text-blue-500">EC2 to EKS</span>
           </h3>
-          {/* <img
-            src="/path/to/eks-image.png"
-            alt="EC2 to EKS"
-            className="mb-4 w-16 h-16"
-          /> */}
-          <p className="text-gray-700 mb-6 text-sm md:pt-12 sm:text-base">
+          <p className="text-gray-700 mb-6 text-sm sm:text-base">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum alias
             et harum neque nisi aliquam impedit deserunt? Est, nesciunt debitis
             mollitia similique dolorem nihil tenetur, error inventore porro
@@ -45,17 +40,12 @@ const Oursuccess = () => {
             Read More
           </a>
         </div>
-        <div className="bg-white rounded-lg shadow-2xl p-6 flex flex-col items-center text-center">
+        <div className="bg-white rounded-xl shadow-2xl p-6 flex flex-col items-center text-center">
           <h3 className="text-lg sm:text-xl font-semibold mb-4">
             Migration from{" "}
             <span className="font-bold text-blue-500">Heroku to GCP</span>
           </h3>
-          {/* <img
-            src="/path/to/gcp-image.png"
-            alt="Heroku to GCP"
-            className="mb-4 w-16 h-16"
-          /> */}
-          <p className="text-gray-700 mb-6 md:pt-12 text-sm sm:text-base">
+          <p className="text-gray-700 mb-6 text-sm sm:text-base">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum alias
             et harum neque nisi aliquam impedit deserunt? Est, nesciunt debitis
             mollitia similique dolorem nihil tenetur, error inventore porro
@@ -69,4 +59,5 @@ const Oursuccess = () => {
     </div>
   );
 };
+
 export default Oursuccess;
--- src/modules/Home/components/Technologies.jsx
+++ src/modules/Home/components/Technologies.jsx
@@ -4,14 +4,15 @@ import TechImg1 from "../../../assets/images/technologies-1-1024x435.png";
 import TechImg2 from "../../../assets/images/technologies-2-1024x435.png";
 import TechImg3 from "../../../assets/images/technologies-3-1024x435.png";
 import TechImg4 from "../../../assets/images/technologies-4-1024x435.png";
+
 const Technologies = () => {
   return (
-    <div className=" bg-[#ffffff] pb-9 md:py-6">
-      <h2 className="text-4xl font-bold leading-relaxed text-center mt-5 ">
+    <div className="bg-white pb-9 md:py-6">
+      <h2 className="text-4xl font-bold leading-relaxed text-center mt-5">
         <span className="text-blue-500">Technologies</span>
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 px-10  gap-y-8 md:px-40  justify-center pt-4 pb-4 ">
-        <div className="flex justify-center items-center ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 px-4 md:px-10 lg:px-20 py-4">
+        <div className="flex justify-center items-center">
           <TechCard text={"Cloud Platforms"} src={TechImg4} />
         </div>
         <div className="flex justify-center items-center">
@@ -27,4 +28,5 @@ const Technologies = () => {
     </div>
   );
 };
+
 export default Technologies;
