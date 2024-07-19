082686755fea783a7433b3b2c1200b14643a0dcd
kunalsinha04 <kunal.sinha32@gmail.com>
2024-07-19 12:05:55 +0530

    Resources Finnally done with responsive

--- src/modules/Resources/components/Card.jsx
+++ src/modules/Resources/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ title, imageUrl, description, buttonText, buttonUrl }) => {
   return (
-    <div className="bg-white shadow-2xl rounded-lg overflow-hidden">
+    <div className="bg-white shadow-2xl rounded-xl overflow-hidden">
       <img className="w-full h-48 object-cover" src={imageUrl} alt={title} />
       <div className="p-6 ">
         <p className="text-gray-700 mb-4">{description}</p>
--- src/modules/Resources/components/CardsDeatil.jsx
+++ src/modules/Resources/components/CardsDeatil.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Card from "./Card";
 import img from "../../../assets/images/MigratingfromECStoEKS-2-.avif";
+
 const cardsData = [
   {
-    title: "Migration from  EC2 to EKS",
+    title: "Migration from EC2 to EKS",
     imageUrl: img,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto suscipit rerum error repellendus nemo obcaecati aliquam omnis, dolorum accusantium officiis ab qui ratione nesciunt natus? Officiis itaque aliquid quae accusantium?",
@@ -11,7 +12,7 @@ const cardsData = [
     buttonUrl: "#",
   },
   {
-    title: "Migration from  EC2 to EKS",
+    title: "Migration from EC2 to EKS",
     imageUrl: img,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto suscipit rerum error repellendus nemo obcaecati aliquam omnis, dolorum accusantium officiis ab qui ratione nesciunt natus? Officiis itaque aliquid quae accusantium?",
@@ -19,7 +20,7 @@ const cardsData = [
     buttonUrl: "#",
   },
   {
-    title: "Migration from  EC2 to EKS",
+    title: "Migration from EC2 to EKS",
     imageUrl: img,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto suscipit rerum error repellendus nemo obcaecati aliquam omnis, dolorum accusantium officiis ab qui ratione nesciunt natus? Officiis itaque aliquid quae accusantium?",
@@ -27,7 +28,7 @@ const cardsData = [
     buttonUrl: "#",
   },
   {
-    title: "Migration from  EC2 to EKS",
+    title: "Migration from EC2 to EKS",
     imageUrl: img,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto suscipit rerum error repellendus nemo obcaecati aliquam omnis, dolorum accusantium officiis ab qui ratione nesciunt natus? Officiis itaque aliquid quae accusantium?",
@@ -35,7 +36,7 @@ const cardsData = [
     buttonUrl: "#",
   },
   {
-    title: "Migration from  EC2 to EKS",
+    title: "Migration from EC2 to EKS",
     imageUrl: img,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto suscipit rerum error repellendus nemo obcaecati aliquam omnis, dolorum accusantium officiis ab qui ratione nesciunt natus? Officiis itaque aliquid quae accusantium?",
@@ -43,7 +44,7 @@ const cardsData = [
     buttonUrl: "#",
   },
   {
-    title: "Migration from  EC2 to EKS",
+    title: "Migration from EC2 to EKS",
     imageUrl: img,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto suscipit rerum error repellendus nemo obcaecati aliquam omnis, dolorum accusantium officiis ab qui ratione nesciunt natus? Officiis itaque aliquid quae accusantium?",
@@ -51,7 +52,7 @@ const cardsData = [
     buttonUrl: "#",
   },
   {
-    title: "Migration from  EC2 to EKS",
+    title: "Migration from EC2 to EKS",
     imageUrl: img,
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto suscipit rerum error repellendus nemo obcaecati aliquam omnis, dolorum accusantium officiis ab qui ratione nesciunt natus? Officiis itaque aliquid quae accusantium?",
@@ -62,17 +63,19 @@ const cardsData = [
 
 const CardsDetail = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 px-10  gap-10 md:px-28  justify-center pt-12 pb-12">
-      {cardsData.map((card, index) => (
-        <Card
-          key={index}
-          title={card.title}
-          imageUrl={card.imageUrl}
-          description={card.description}
-          buttonText={card.buttonText}
-          buttonUrl={card.buttonUrl}
-        />
-      ))}
+    <div className="container mx-auto px-4 py-12 px-12">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  xl:grid-cols-4 gap-10">
+        {cardsData.map((card, index) => (
+          <Card
+            key={index}
+            title={card.title}
+            imageUrl={card.imageUrl}
+            description={card.description}
+            buttonText={card.buttonText}
+            buttonUrl={card.buttonUrl}
+          />
+        ))}
+      </div>
     </div>
   );
 };
