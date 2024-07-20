977540e3da93ac0979bad925747ebbf1ec61a7cb
Tapan Sahu <sahutapan320@gmail.com>
2024-07-20 19:57:17 +0530

    feat: add one more section

--- src/modules/Devops/components/DevopsTeam.jsx
+++ src/modules/Devops/components/DevopsTeam.jsx
@@ -3,15 +3,11 @@ import TeamCard from "./TeamCard";
 
 const DevOpsTeam = () => {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center p-6 space-y-6 md:space-y-0 md:space-x-6">
-      <div className="flex-shrink-0 w-full md:w-2/3 lg:w-3/5">
-        <img
-          src="https://iamops.io/wp-content/uploads/2022/05/DevOps-5-1024x717.png"
-          alt="DevOps Team"
-          className="w-full h-auto rounded-lg" // Removed border
-        />
-      </div>
-      <div className="flex flex-col items-center md:items-start space-y-6 w-full md:w-1/3 lg:w-2/5">
+    <>
+      <h2 className="text-center md:text-[60px] leading-[32px] font-bold my-9 text-[36px]">
+        Your DevOps Team
+      </h2>
+      <div className="flex items-center justify-center md:items-start w-full gap-7 flex-wrap">
         <TeamCard
           icon="https://iamops.io/wp-content/uploads/2022/05/DevOps-5-1024x717.png"
           title="Head Of DevOps"
@@ -28,7 +24,7 @@ const DevOpsTeam = () => {
           description="Dedicated to your tech team and business values. They act as the main point of contact for your DevOps Team."
         />
       </div>
-    </div>
+    </>
   );
 };
 
